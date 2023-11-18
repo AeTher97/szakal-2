108daffb1a7b13cf33be28414f8a415b3ab19c80
Michal Wozniak <michael93509@gmail.com>
2023-11-19 00:44:24 +0100

    Fix port picking up in heroku dyno

--- backend/src/main/java/org/iaeste/szakal2/SzakalBackend.java
+++ backend/src/main/java/org/iaeste/szakal2/SzakalBackend.java
@@ -18,6 +18,12 @@ public class SzakalBackend {
     }
 
     public static void main(String[] args) {
+        String ENV_PORT = System.getenv().get("PORT");
+        String ENV_DYNO = System.getenv().get("DYNO");
+        if (ENV_PORT != null && ENV_DYNO != null) {
+            System.getProperties().put("server.port", ENV_PORT);
+        }
+
         SpringApplication.run(SzakalBackend.class, args);
     }
 
