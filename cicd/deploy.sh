5e58f1e359485513ea39ef0f80b69deaff448303
Michal Wozniak <michael93509@gmail.com>
2023-11-19 02:15:38 +0100

    Update java download link

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -17,8 +17,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 #Install java
 sudo apt install wget
-wget https://download.java.net/java/early_access/jdk21/28/GPL/openjdk-21-ea+xx_linux-x64_bin.tar.gz
-tar -xvf openjdk-21-ea+28_linux-x64_bin.tar.gz
+wget https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz
+tar -xvf openjdk-21.0.1_linux-x64_bin.tar.gz
 cd jdk-21
 sudo mkdir -p /usr/local/jdk-21
 sudo mv * /usr/local/jdk-21
