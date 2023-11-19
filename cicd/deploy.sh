0cb5020b13a77fb892bd41d5472d147d376a1191
Michal Wozniak <michael93509@gmail.com>
2023-11-19 02:42:27 +0100

    Set java home for maven inline

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -20,7 +20,8 @@ sudo apt install wget
 wget https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz
 tar -xvf openjdk-21.0.1_linux-x64_bin.tar.gz
 
-sudo JAVA_HOME=/home/runner/work/szakal-2/szakal-2/jdk-21.0.1 java -v
+java -version
+cat /etc/java/maven.conf
 
 # Launch backend integration test
 cd backend
