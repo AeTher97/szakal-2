9dbce72bf274a1f4b82b8abefa89e130394d1c3d
Michal Wozniak <michael93509@gmail.com>
2024-11-10 13:39:22 +0100

    Contact events visual enhancements

--- frontend/src/components/timline/Timeline.jsx
+++ frontend/src/components/timline/Timeline.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import TimelineArrow from "./TimelineArrow";
 
 const Timeline = ({children}) => {
     return (
         <div style={{paddingTop: 10, paddingBottom: 10}}>
-            <TimelineArrow/>
             {children}
         </div>
     );
