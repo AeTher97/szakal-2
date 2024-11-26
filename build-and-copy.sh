25b0389e7498885d932b9c88725a89a0e9816dda
Michal Wozniak <michael93509@gmail.com>
2024-11-26 16:55:38 +0100

    Install node 22 to avoid potential issues with new major versions

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -5,7 +5,7 @@ if $1 -eq true; then {
   sudo npm cache clean -f
   sudo npm install -g n
   sudo n stable
-  sudo n latest
+  sudo n 22
 } fi;
 CYPRESS_CACHE_FOLDER=./tmp/Cypress npm install
 npm run build
