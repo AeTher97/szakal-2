f3c6a4b66738b4cf63884ea6d7932a246bde3961
Michal Wozniak <michael93509@gmail.com>
2025-02-17 01:22:18 +0100

    Add logging to database reload

--- reload-database.sh
+++ reload-database.sh
@@ -8,7 +8,14 @@ fi;
 
 docker run --name postgres-end-to-end -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=szakal -v "/$PWD/frontend/cypress/init.sql:/docker-entrypoint-initdb.d/init.sql" -d --network=szakal-network postgres
 
-until [ "$(docker inspect -f '{{.State.Running}}' postgres-end-to-end)" == "true" ]; do
+counter=0
+postgresUp=false
+until [ $counter -gt 5 ]; do
   echo "Waiting for postgres to come online"
+  ((counter++))
   sleep 1;
 done;
+
+if  [ "$(docker inspect -f '{{.State.Running}}' postgres-end-to-end)" == "true" ] || [ $postgresUp != true ]; then
+  docker logs postgres-end-to-end
+fi;
