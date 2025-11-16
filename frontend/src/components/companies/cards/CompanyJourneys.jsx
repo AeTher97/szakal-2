678f85f38d6a4390c5b660aa88033c89e7215a9a
Michal Wozniak <michael93509@gmail.com>
2025-11-16 16:23:05 +0100

    Small visual improvements

--- frontend/src/components/companies/cards/CompanyJourneys.jsx
+++ frontend/src/components/companies/cards/CompanyJourneys.jsx
@@ -15,24 +15,29 @@ const CompanyJourneys = ({localCompany}) => {
                     <Typography level={"body-sm"}>Historia poprzednich kontaktów</Typography>
                 </CardContent>
                 <Divider inset={"context"}/>
-                <CardContent>
-                    {localCompany.contactJourneys.map((journey, i) => <div key={journey.id}
-                                                                           style={{
-                                                                               display: "flex",
-                                                                               flexDirection: "row",
-                                                                               flexWrap: "wrap",
-                                                                               gap: 10
-                                                                           }}>
-                        {journey.user ?
-                            <UserAvatar id={journey.user.id} name={journey.user.name} email={journey.user.email}
-                                        surname={journey.user.surname} committee={journey.user.committee}
-                                        size={"sm"} overrideMobile={true}/>
-                            : "Brak przypisanego użytkownika"}
-                        {i !== localCompany.contactJourneys.length - 1 && <Divider inset={"context"}/>}
-                        <LinkWithRouter
-                            to={`/secure/journeys/${journey.id}`}>{journey.campaign.name} - {contactStatusUtils(journey.contactStatus)}
-                        </LinkWithRouter>
-                    </div>)}
+                <CardContent style={{paddingTop: 0}}>
+                    {localCompany.contactJourneys.map((journey, i) =>
+                        <div key={journey.id}>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    flexWrap: "wrap",
+                                    gap: 5,
+                                    paddingBottom: 10,
+                                    paddingTop: 10
+                                }}>
+                                {journey.user ?
+                                    <UserAvatar id={journey.user.id} name={journey.user.name} email={journey.user.email}
+                                                surname={journey.user.surname} committee={journey.user.committee}
+                                                size={"sm"} overrideMobile={true}/>
+                                    : "Brak przypisanego użytkownika"}
+                                <LinkWithRouter
+                                    to={`/secure/journeys/${journey.id}`}>{journey.campaign.name} - {contactStatusUtils(journey.contactStatus)}
+                                </LinkWithRouter>
+                            </div>
+                            {i !== localCompany.contactJourneys.length - 1 && <Divider inset={"context"}/>}
+                        </div>)}
                 </CardContent>
             </CardOverflow>
         </Card>
