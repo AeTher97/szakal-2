145bcb050accb99418e72d432f79c75cf7f4ac43
Michal Wozniak <michael93509@gmail.com>
2025-11-25 23:00:02 +0100

    Bump gradle, fix imports

--- backend/src/main/java/org/iaeste/szakal2/security/handlers/SzakalRefreshSuccessHandler.java
+++ backend/src/main/java/org/iaeste/szakal2/security/handlers/SzakalRefreshSuccessHandler.java
@@ -1,6 +1,5 @@
 package org.iaeste.szakal2.security.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -9,6 +8,7 @@ import org.iaeste.szakal2.security.RefreshTokenAuthentication;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
